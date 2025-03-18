fa5c86acc38a0b8cf696c4855f83ece5d0415cf0
Hoàng Đức <dieuthanki123@gmail.com>
2025-03-18 13:40:08 +0700

    add product

---
+++ DauAnNganNam/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using DauAnNganNam.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DauAnNganNam.Controllers
+{
+    public class ProductController : Controller
+    {
+        public readonly ExeDBContext _context;
+
+        public ProductController(ExeDBContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+        {
+            var queryProduct = _context.Products.Include(x => x.Images).Include(x => x.ProductCategory).ToList();
+            var queryCategory = _context.ProductCategories.ToList();
+			ViewBag.Categories = queryCategory;
+			ViewBag.Products = queryProduct;
+			return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var queryProduct = _context.Products.Include(x => x.Images).Include(x => x.ProductCategory).FirstOrDefault(x => x.ProductId == id);
+            ViewBag.Product = queryProduct;
+            return View();
+        }
+    }
+}
