d0066045500f4d9fc107324bc0b3624676f5f8b7
Hoàng Đức <dieuthanki123@gmail.com>
2025-03-24 22:06:23 +0700

    ud

--- DauAnNganNam/Controllers/HomeController.cs
+++ DauAnNganNam/Controllers/HomeController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DauAnNganNam.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DauAnNganNam.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly ExeDBContext _context;
+        public HomeController(ExeDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var products = _context.Products.Include(x => x.Images);
+            ViewBag.Products = products;
             return View();
         }
     }
